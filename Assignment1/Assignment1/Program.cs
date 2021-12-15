a8969ae2c51489408b0886a9ca1be53a6963a1d7
Florin Lazar <florin.lazar.it2014@gmail.com>
2021-12-15 22:09:20 +0200

    Solve assignment 1

---
+++ Assignment1/Assignment1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("Products.txt");
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(new[] { "|" }, StringSplitOptions.None);
+                foreach(string part in parts)
+                {
+                    Console.Write($"{part, -10}");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
